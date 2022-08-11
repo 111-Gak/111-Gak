f72fba27bb3f274e46472597eb8df327212a42b3
Dusuna <dusunax@gmail.com>
2022-08-11 19:15:59 +0900

    220811 bugfix - 19:00

--- src/store/modules/checklist.js
+++ src/store/modules/checklist.js
@@ -1,47 +1,22 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     checklist: {
         chk1Text: ""
     }
 }
-export const patchChecklistThunk = createAsyncThunk(
-    "patch",
-    async (payload, api) => {
-        console.log(payload, api);
-        try {
-            const data = await axios.patch("http://localhost:3001/checklist?postId=6f-s9ewv0n", payload)
-            return data
-            // return api.fulfillWithValue(data.data)
-        } catch(err) {
-            return api.rejectWithValue(err)
-        }
-    } 
-)
 
 export const checklistSlice = createSlice({
     name: "checklists",
-    initialState: { list: [{ id: '3423423423t6sd', chkchk: 0 }] },
+    initialState,
     reducers: {
         editChecklist: (state, action) => {
-            const {idx: idx, currentValue} = action.payload;
-            const newState={...state.checklist, "chkText": currentValue, idx: idx};
-            
-            // console.log(newState);
+            const {currentValue, name} = action.payload;
+            const currText = name + "Text"
+            const newState = {...state.checklist, [name]: 0, [currText]: currentValue};
 
             state.checklist = newState;
-        },
-        patchChecklist: (state, action) => {
         }
-    },
-    extraReducers: (builder) => {
-        builder.addCase(patchChecklistThunk.fulfilled, (state, action) => {
-            const newList = state.list.map((el) => {
-                return el.id === action.payload.id ? action.payload : el;
-            })
-            state.list = newList;
-        })
     }
 })
 
